e3eed3f489d38830367db60b370f6ec331c7e374
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-02-07 10:47:49 -0300

    feat: create header, loading and layout component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,10 @@
+import { Header } from '@/components/header'
 import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-white">
-        Hello React Native!
-      </Text>
+    <View className="flex-1 pt-8">
+      <Header title="Cardápio"/>
     </View>
   )
 }
