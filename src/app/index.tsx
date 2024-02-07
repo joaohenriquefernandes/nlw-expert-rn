355756e57de62af87324f8aadd18eee66b0928c9
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-02-07 11:04:20 -0300

    feat: update header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Header title="Cardápio"/>
+      <Header title="Cardápio" cartQuantityItems={3}/>
     </View>
   )
 }
