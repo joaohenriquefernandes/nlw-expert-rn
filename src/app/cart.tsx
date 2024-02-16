2ce5dfdbb8a5f14b22168ef4435349ba0ebf6582
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-02-16 11:22:42 -0300

    feat: create functionality to send via whatsapp

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -6,12 +6,19 @@ import { Product } from "@/components/product";
 import { ProductCartProps, useCartStore } from "@/stores/cart-store";
 import { formatCurrency } from "@/utils/functions/format-currency";
 import { Feather } from "@expo/vector-icons";
-import { Alert, ScrollView, Text, View } from "react-native";
+import { useNavigation } from "expo-router";
+import { useState } from "react";
+import { Alert, ScrollView, Text, View, Linking } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
+const PHONE_NUMBER="+5535997151223"
 
 export default function Cart() {
   const cartStore = useCartStore()
+
+  const [address, setAddress] = useState("")
+
+  const navigation = useNavigation()
 
   const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0))
 
@@ -23,7 +30,28 @@ export default function Cart() {
         onPress: () => cartStore.remove(product.id)
       }
     ])
+  }
 
+  function handleOrder() {
+    if(address.trim().length === 0){
+      return Alert.alert('Pedido', "Informe os dados da entrega.")
+    }
+
+    const products = cartStore.products.map((product) => `\n ${product.quantity} ${product.title}`).join("")
+
+    const message = `
+      🍔 NOVO PEDIDO
+      \n Entregar em: ${address}
+
+      ${products}
+
+      \n Valor total: ${total}
+    `
+
+    Linking.openURL(`http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`)
+
+    cartStore.clear()
+    navigation.goBack()
   }
 
   return (
@@ -56,13 +84,19 @@ export default function Cart() {
               <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
             </View>
 
-            <Input placeholder="Informe o endereço de entrega..."/>
+            <Input
+              placeholder="Informe o endereço de entrega..."
+              onChangeText={setAddress}
+              blurOnSubmit={true}
+              onSubmitEditing={handleOrder}
+              returnKeyType="next"
+            />
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
 
       <View className="p-5 gap-5">
-        <Button>
+        <Button onPress={handleOrder}>
           <Button.Text>Enviar pedido</Button.Text>
           <Button.Icon>
             <Feather name="arrow-right-circle" size={20} />
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -13,6 +13,7 @@ type StateProps = {
   products: ProductCartProps[]
   add: (product: ProductProps) => void
   remove: (productId: string) => void
+  clear: () => void
 }
 
 export const useCartStore = create(persist<StateProps>((set) => ({
@@ -24,7 +25,9 @@ export const useCartStore = create(persist<StateProps>((set) => ({
 
   remove: (productId: string) => set((state) => ({
     products: cartInMemory.remove(state.products, productId)
-  }))
+  })),
+
+  clear: () => set(() => ({products: []}))
 }), {
   name:"nlw-expert:cart",
   storage: createJSONStorage(() => AsyncStorage)
